01d879f56fb74ad74b77d2b19eeb5826822ce660
AxeyoS <154218653+AxeyoS@users.noreply.github.com>
2024-06-12 21:21:20 +0900

    Practice 6

---
+++ script.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+        items: [
+            { id: 1, image: 'image1.jpg', description: 'Описание 1' },
+            { id: 2, image: 'image2.jpg', description: 'Описание 2' },
+            { id: 3, image: 'image3.jpg', description: 'Описание 3' },
+            { id: 4, image: 'image4.jpg', description: 'Описание 4' },
+            { id: 5, image: 'image5.jpg', description: 'Описание 5' },
+            { id: 6, image: 'image6.jpg', description: 'Описание 6' },
+        ]
+    }
+});
